414d6cc8b431f62144a6186335ff530f26c45369
Deeksha Agarwal <agarwaldeeksha03@gmail.com>
2023-04-23 23:44:20 +0530

    added the UI for HomeActivity.kt

--- app/src/main/java/com/example/socialhive/HomeActivity.kt
+++ app/src/main/java/com/example/socialhive/HomeActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.socialhive.databinding.ActivityHomeBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class HomeActivity : AppCompatActivity() {
 // again instantiate these vars
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityHomeBinding
+
+    // instantiating var for database reference
+    private lateinit var dbReference: DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +32,16 @@ class HomeActivity : AppCompatActivity() {
         val gmail = intent.getStringExtra("gmail")
         val gProfile = intent.getStringExtra("gProfile")
 
-        // changing the text to show the name of user got from the google account
-        binding.textView.text = gName
+        // getting only the first name of the user
+        val gNameParts = gName?.split(" ")
+        val gFirstName = gNameParts?.get(0)
+
+        // changing the text to Welcome "first name"
+        binding.userName.text = "Welcome $gFirstName"
+
+        // initializing dbReference, adding reference which will show in
+        // db and we can use it later to retrieve data
+        dbReference = FirebaseDatabase.getInstance().getReference("Users")
+
     }
 }
